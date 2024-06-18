cc6d09b4d93e876226916d6120a25eef7475fb49
Nico Wegner <nicowgr@gmail.com>
2024-06-18 08:57:17 +0200

    Added tooltips to tool buttons

--- src/components/PergamentToolbar.tsx
+++ src/components/PergamentToolbar.tsx
@@ -14,15 +14,20 @@ const PergamentToolbar = ({ noteFunctions }: PergamentToolbarProps) => {
       {noteFunctions.map((note) => {
         const Icon = note.icon;
         return (
-          <button
-            title={"Add " + note.name}
-            className="shadow-md bg-zinc-100 text-zinc-800 hover:bg-zinc-800 hover:text-zinc-100 border-2 border-zinc-800 font-bold py-2 px-4 m-2 w-16 h-16 rounded-2xl z-[1000]"
-            onClick={note.createFunction}
-            key={note.name}
-          >
-            <Icon className="w-full h-full" />
-            {/* <span>{note.name}</span> */}
-          </button>
+          <span className="group relative z-[1000]">
+            <div className="absolute hidden left-[calc(100%)] top-[calc(50%-1rem)] group-hover:block w-auto select-none">
+              <div className="bottom-full rounded bg-zinc-800 px-4 py-1 text-xs text-white whitespace-nowrap">
+                {note.name}
+              </div>
+            </div>
+            <button
+              className="tool shadow-md bg-zinc-100 text-zinc-800 hover:bg-zinc-800 hover:text-zinc-100 border-2 border-zinc-800 font-bold py-2 px-4 m-2 w-16 h-16 rounded-2xl"
+              onClick={note.createFunction}
+              key={note.name}
+            >
+              <Icon className="w-full h-full" />
+            </button>
+          </span>
         );
       })}
     </div>
