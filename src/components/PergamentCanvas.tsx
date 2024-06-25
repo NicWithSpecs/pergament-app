43ece061fd2b63144a902ea1b8ad0989d82f96d1
Nico Wegner <nicowgr@gmail.com>
2024-06-25 09:01:15 +0200

    Increased zoom range

--- src/components/PergamentCanvas.tsx
+++ src/components/PergamentCanvas.tsx
@@ -215,8 +215,8 @@ const PergamentCanvas = () => {
       <div className="h-full w-screen overflow-hidden bg-zinc-200 text-zinc-900 transition-colors duration-500 dark:bg-zinc-900 dark:text-zinc-200">
         <ReactFlow
           onInit={onInit}
-          maxZoom={1.8}
-          minZoom={1}
+          maxZoom={2}
+          minZoom={0.6}
           nodes={nodes}
           edges={edges}
           onNodesChange={onNodesChange}
