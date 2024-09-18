8f3b0d63a428d8d36d09946040c28123615943b7
Nico Wegner <nicowgr@gmail.com>
2024-09-18 23:38:23 +0200

    Adjusted breakpoints for landing page

--- src/pages/PergamentLandingPage.tsx
+++ src/pages/PergamentLandingPage.tsx
@@ -9,7 +9,7 @@ const PergamentLandingPage = () => {
       <LPNavbar />
 
       <div className="flex flex-col items-center">
-        <div className="mt-10 flex flex-col items-center p-10 md:mt-14 lg:mt-16">
+        <div className="mt-10 flex flex-col items-center md:mt-14 lg:mt-16">
           <div className="bg-gradient-to-t from-zinc-900 to-zinc-400 bg-clip-text p-2 text-5xl font-medium tracking-tighter text-transparent md:text-6xl lg:text-8xl">
             Get organized.
           </div>
@@ -18,19 +18,19 @@ const PergamentLandingPage = () => {
           </div>
         </div>
 
-        <div className="m-2 max-w-xl text-center text-lg tracking-tight md:m-4 md:max-w-2xl md:text-xl lg:m-6 lg:max-w-4xl lg:text-2xl">
+        <div className="m-8 max-w-xl text-center text-lg tracking-tight md:m-8 md:max-w-2xl md:text-xl lg:m-12 lg:max-w-4xl lg:text-2xl">
           Pergament is a leightweight tool for brainstorming, taking notes and
           organizing.
         </div>
         <Link
           to="/whiteboard"
-          className="space-evenly m-10 inline-flex justify-center whitespace-nowrap rounded-2xl border border-zinc-300 bg-zinc-900 px-8 py-3 text-xl font-medium text-zinc-100 shadow-lg hover:bg-zinc-100 hover:text-zinc-900 dark:bg-zinc-100 dark:text-zinc-900"
+          className="space-evenly inline-flex justify-center whitespace-nowrap rounded-2xl border border-zinc-300 bg-zinc-900 px-8 py-3 text-lg font-medium text-zinc-100 shadow-lg hover:bg-zinc-100 hover:text-zinc-900 lg:text-xl dark:bg-zinc-100 dark:text-zinc-900"
         >
           Get started
         </Link>
         <img
           src={screenshot}
-          className="m-6 w-[90%] drop-shadow-2xl lg:w-[70%]"
+          className="mt-14 w-[90%] drop-shadow-2xl lg:w-[70%]"
         />
       </div>
     </>
